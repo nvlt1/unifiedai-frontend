b67432ad05cec57f2a94cb4003748711acc2f898
Nav <navdeepnx@gmail.com>
2024-10-26 18:46:48 -0700

    updated landing page, missing background images

--- oneidfrontend/src/pages/LandingPage.js
+++ oneidfrontend/src/pages/LandingPage.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.scss';
+// import backgroundImage from '../../assets/background.png'; get the image and plug it into assets folder eventually
 
 const LandingPage = () => {
     return (
-        <div clsasName="landing-container">
-            <h1>Welcome to OneID</h1>
-            <p>UnifiedAI Technological Solutions offers a seamless identity management platform</p>
+        <div className="landing-container"
+            //  style={{ backgroundImage: `url(${backgroundImage})` }}
+            >
+            
+            <h1 className="main-heading">OneID New Experience</h1>
+            <p className="sub-heading">UnifiedAI Technological Solutions</p>
             <Link to="/create-id" className="create-id-button">
             Create Your OneID
             </Link>
+            <div className="service-intro">
+                <h2>Short service introductions here</h2>
+                <p>Step by Step</p>
+            </div>
+            <footer className="footer">
+                <p>End Footnote</p>
+            </footer>
         </div>
     );
 };
