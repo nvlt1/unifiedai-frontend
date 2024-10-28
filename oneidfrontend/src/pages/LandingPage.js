217f5bd82218ce7f2e9462cf49be221e6b486bdc
Nav <navdeepnx@gmail.com>
2024-10-27 17:03:46 -0700

    modified landing page design

--- oneidfrontend/src/pages/LandingPage.js
+++ oneidfrontend/src/pages/LandingPage.js
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import './LandingPage.scss';
 // import backgroundImage from '../../assets/background.png'; get the image and plug it into assets folder eventually
 
+
 const LandingPage = () => {
     return (
         <div className="landing-container">
-            
-            <h1 className="main-heading">OneID New Experience</h1>
-            <p className="sub-heading">UnifiedAI Technological Solutions</p>
-            <Link to="/create-id" className="create-id-button">
-            Create Your OneID
-            </Link>
+            <div className="intro-section">
+                <h1 className="main-heading">OneID New Experience</h1>
+                <p className="sub-heading">UnifiedAI Technological Solutions</p>
+                <Link to="/create-id" className="create-id-button">
+                    Create Your OneID
+                </Link>
+            </div>
             <div className="service-intro">
                 <h2>Short service introductions here</h2>
                 <p>Step by Step</p>
@@ -22,5 +24,27 @@ const LandingPage = () => {
         </div>
     );
 };
+
+
+
+// const LandingPage = () => {
+//     return (
+//         <div className="landing-container">
+            
+//             <h1 className="main-heading">OneID New Experience</h1>
+//             <p className="sub-heading">UnifiedAI Technological Solutions</p>
+//             <Link to="/create-id" className="create-id-button">
+//             Create Your OneID
+//             </Link>
+//             <div className="service-intro">
+//                 <h2>Short service introductions here</h2>
+//                 <p>Step by Step</p>
+//             </div>
+//             <footer className="footer">
+//                 <p>End Footnote</p>
+//             </footer>
+//         </div>
+//     );
+// };
 
 export default LandingPage;
